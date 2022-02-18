a78cb9f6ae594a451558d77708aaefb81e1a76e5
Mario-ECS <mcontreras.se777@gmail.com>
2022-02-18 00:05:52 -0600

    feature: simplifying new fundraiser form code

--- client/services/fundraisers.ts
+++ client/services/fundraisers.ts
@@ -4,33 +4,68 @@ import FactoryContract from '../contracts/Factory.json'
 /* Utils */
 import { getWeb3 } from '../utils/scripts'
 
+interface Fundraiser {
+  contract_name: string,
+  contract_website: string,
+  contract_image: string,
+  contract_description: string,
+  contract_beneficiary: string,
+  contract_owner: string,
+}
+
+const getAccountsAndContracts = async () => {
+  const web3 = await getWeb3()
+  const networkId = await web3.eth.net.getId()
+  const accounts = await web3.eth.getAccounts()
+  const deployedNetwork = FactoryContract.networks[networkId]
+  const contract = new web3.eth.Contract(
+    FactoryContract.abi,
+    deployedNetwork && deployedNetwork.address,
+  )
+
+  return { accounts, contract }
+}
+
+
 const getFundraisers = ({ getBy, offset }: { getBy: number, offset: number }) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const web3 = await getWeb3()
-      const networkId = await web3.eth.net.getId()
-      const accounts = await web3.eth.getAccounts()
-      const funTotal = parseFloat(web3.utils.fromWei(await web3.eth.getBalance(accounts[0]), 'Ether')).toFixed(2)
-      const deployedNetwork = FactoryContract.networks[networkId]
-      const contract = new web3.eth.Contract(
-        FactoryContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      )
-
+      const { utils, eth } = await getWeb3()
+      const { accounts, contract } = await getAccountsAndContracts()
+      const funTotal = parseFloat(utils.fromWei(await eth.getBalance(accounts[0]), 'Ether')).toFixed(2)
       const fundraisers = await contract.methods.fundraisers(getBy, offset).call()
 
       resolve({
-        contract,
         accounts,
         fundraisers,
         funTotal
       })
     } catch (error) {
-      reject('Failed to load web3, accounts, or contract. Check console for details.')
+      reject(error)
+    }
+  })
+}
+
+const createFundraiser = (fundraiser: Fundraiser) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { contract } = await getAccountsAndContracts()
+      await contract.methods.createFundraiser(
+        fundraiser.contract_name,
+        fundraiser.contract_website,
+        fundraiser.contract_image,
+        fundraiser.contract_description,
+        fundraiser.contract_beneficiary,
+      ).send({ from: fundraiser.contract_owner })
+
+      resolve('Fundraiser created')
+    } catch (error) {
+      reject(error)
     }
   })
 }
 
 export {
-  getFundraisers
+  getFundraisers,
+  createFundraiser
 }
