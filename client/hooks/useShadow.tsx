5e9aba3c537896d65fec59a00e51b1c916183bc5
Mario-ECS <mcontreras.se777@gmail.com>
2022-03-05 01:10:47 -0600

    feature: adding date picker to create fundraiser form

--- client/hooks/useShadow.tsx
+++ client/hooks/useShadow.tsx
@@ -3,23 +3,25 @@ import { useState } from 'react'
 
 /* Modules */
 import { useSpring } from 'react-spring'
-import { SHADOWS, TRANSITIONS } from '../utils/styles_constants'
+import { SHADOWS } from '../utils/styles_constants'
 
 function useShadow({
   springConfig = {
-    mass: 1.1, tension: 240, friction: 13
-  },
+    mass: 1.1,
+    tension: 240,
+    friction: 13
+  }
 }) {
   const [isHovered, setIsHovered] = useState(false)
   const shadowStyle = useSpring({
+    config: springConfig,
     to: { 
       boxShadow: isHovered ? SHADOWS.medium : SHADOWS.small,
       transform: isHovered ? 'scale(1.00666)' : 'scale(1)'
-    },
-    config: springConfig,
+    }
   })
 
-  const triggerShadow = type => setIsHovered(type);
+  const triggerShadow = (type: boolean) => setIsHovered(type)
   return [shadowStyle, triggerShadow]
 }
 export default useShadow
